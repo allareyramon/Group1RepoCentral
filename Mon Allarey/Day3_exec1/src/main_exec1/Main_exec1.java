d9d6165a19ed6a724872dbc2d805791fc479408e
BARISJA2 <BARISJA2@ZHA-ITA016-W7.corp.oocl.com>
2017-08-02 09:55:26 +0800

    Matt's Test Push

--- Mon Allarey/Day3_exec1/src/main_exec1/Main_exec1.java
+++ Mon Allarey/Day3_exec1/src/main_exec1/Main_exec1.java
@@ -4,6 +4,6 @@ public class Main_exec1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("This is Matt");
 	}
 }
