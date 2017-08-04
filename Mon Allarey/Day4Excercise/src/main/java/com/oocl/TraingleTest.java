ad4e074aff2d219b29c9c71c22372aa558d2a431
ALLARRA <ALLARRA@ZHA-ITA055-W7.corp.oocl.com>
2017-08-04 11:19:33 +0800

    test

--- Mon Allarey/Day4Excercise/src/main/java/com/oocl/TraingleTest.java
+++ Mon Allarey/Day4Excercise/src/main/java/com/oocl/TraingleTest.java
@@ -1,6 +1,5 @@
 package com.oocl;
 
-import java.util.Scanner;
 
 public class TraingleTest {
 
@@ -12,15 +11,13 @@ public class TraingleTest {
 		if(x>0) {
 			sideIterate(y);
 			System.out.println();
-			Downiterate(x-1,y+1);	
-
+			Downiterate(x-1,y+1);
 		}
 	}
 	
 	
 	int val= 10;
 	public void sideIterate(int z){
-		int val =0;
 		if(z>=0) {
 		System.out.print("* ");
 		sideIterate(z-1);
--- Mon Allarey/Day4Excercise/src/test/java/com/oocl/apptest.java
+++ Mon Allarey/Day4Excercise/src/test/java/com/oocl/apptest.java
@@ -1,6 +1,6 @@
 package com.oocl;
 
-import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class apptest {
